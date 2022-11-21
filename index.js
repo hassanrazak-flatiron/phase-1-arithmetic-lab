61d01d30d6d81752d2805e58af486eed6282696d
Hassan Razak <hassanrazak1991@gmail.com>
2022-11-20 22:40:38 -0500

    commit tree 5a0cf653e66ea1656f4dc6e39bc7bc9be24a9921

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let multiply, num1, num2;
+num1 = 2;num2=31;
+multiply = num1*num2;
+
+let random;
+
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod, num3, num4;
+num3 = 22;
+num4 =6;
+mod = num3 % num4
+
+let max;
+max=Math.max(1,2,3,4,20);
